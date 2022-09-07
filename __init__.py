b5bd8c330cd48663d04ed33b56e39b7c5a350861
Anton Lidblad <anton@lidblad.com>
2022-09-07 19:00:54 +0200

    test config-flow

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-"""Example of a custom component exposing a service."""
+"""Custom component Charge Calculator."""
 from __future__ import annotations
 
 import logging
@@ -6,16 +6,14 @@ import logging
 from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.helpers.typing import ConfigType
 
-# The domain of your component. Should be equal to the name of your component.
 DOMAIN = "charge_calculator"
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the an async service example component."""
+    """Set up the an async service charge_calculator."""
     @callback
     def calculate_charge_time(call: ServiceCall) -> None:
-        """My first service."""
+        """Calculate when to charge."""
         _LOGGER.info(f"Received data, {call.data}")
 
     # Register our service with Home Assistant.
