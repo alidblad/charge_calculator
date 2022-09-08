5e20ff25ebd0494dcf56d01a4bde19357276aff7
Anton Lidblad <anton@lidblad.com>
2022-09-08 09:57:11 +0200

    test atributes

--- __init__.py
+++ __init__.py
@@ -17,10 +17,10 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         _LOGGER.info(f"Nordpol entity={config[DOMAIN]['nordpol_entity']}")
         _LOGGER.info(f"Wether entity={config[DOMAIN]['wether_entity']}")
         nordpol_state = hass.states.get(config[DOMAIN]['nordpol_entity'])
-        _LOGGER.info(f"state nordpol name={nordpol_state}")
         name = nordpol_state.name
+        np = nordpol_state.as_dict
         _LOGGER.info(f"state nordpol name={name}")
-        _LOGGER.info(f"atributes nordpol={nordpol_state.as_dict}")
+        _LOGGER.info(f"atributes nordpol={np['today']}")
 
 
     # Register our service with Home Assistant.
